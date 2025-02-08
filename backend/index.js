ea236600cd1fe15d0560b6fd06d500541698de6c
Vighnesh VS <vighneshvs27@gmail.com>
2025-02-08 17:19:01 +0530

    With Pagination, Loader, Search

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
-import cors from 'cors';
+import cors from "cors";
 
 const port = process.env.PORT;
 const app = express();
@@ -12,7 +12,7 @@ app.use(cors());
 
 //To post a new Book
 app.post("/books", async (req, res) => {
-  try{
+  try {
     if (!req.body.title || !req.body.author || !req.body.publishYear) {
       return res.status(404).send("Required data not sent");
     }
@@ -24,78 +24,96 @@ app.post("/books", async (req, res) => {
 
     const book = await Book.create(newBook);
     return res.status(201).send(book);
-  }catch(error){
+  } catch (error) {
     console.log(error.message);
     res.status(500).send({ message: error.message });
   }
 });
 
-//To get all the books in the database
-app.get('/books', async (req, res) => {
-    try{
-        const books = await Book.find({});
-        res.status(200).json({
-            totalCount : books.length,
-            dataList : books
-        });
-    }catch(error){
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-})
+//To get all the books in the databas
+app.get("/books", async (req, res) => {
+  const page = req.query.page;
+  const limit = req.query.limit;
+  const search = req.query.search;
+
+  const skip = (page - 1) * limit;
+  try {
+    const query = search?.length
+      ? { title: { $regex: search, $options: "i" } }
+      : {};
+
+    const totalCountPrm = Book.find(query).countDocuments();
+    const booksPrm = Book.find(query).sort({ _id: 1 }).limit(limit).skip(skip);
+
+    const [totalCount, books] = await Promise.all([totalCountPrm, booksPrm]);
+
+    res.status(200).json({
+      totalCount,
+      dataList: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
 
 //To get one book by id from the database
-app.get('/books/:id', async (req, res) => {
-    try{
-        const {id} = (req.params);
-        
-        const book = await Book.findById(id);
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
 
-        res.status(200).json(book);
-    }catch(error){
-        console.log(error.message);
-        res.status(500).json({message : error.message});
-    }
-})
+    const book = await Book.findById(id);
+
+    res.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
 
 //To update an already existing book by id
-app.put('/books/:id', async (req, res) => {
-    try{
-        if(!req.body.title || !req.body.author || !req.body.publishYear){
-            return res.status(404).json( {message : "Enter the required fields : Title, author, publish year"} );
-        }
-    
-        const {id} = req.params;
-        const book = await Book.findByIdAndUpdate(id, req.body);
-        console.log(book);
-
-        if(!book){
-            return res.status(404).json( {message : `The book with id : ${id} is not found`});
-        }
-        return res.status(200).json({message : 'Book updated'});
-    }catch(error){
-        console.log(error.message);
-        return res.status(500).json({message : error.message});
+app.put("/books/:id", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res
+        .status(404)
+        .json({
+          message: "Enter the required fields : Title, author, publish year",
+        });
     }
-})
 
-//To delete a book by id 
-app.delete('/books/:id', async (req, res) => {
-    try{
-        const {id} = req.params;
-        
-        const books = await Book.findByIdAndDelete(id);
+    const { id } = req.params;
+    const book = await Book.findByIdAndUpdate(id, req.body);
+    console.log(book);
 
-        if(!books){
-            return res.status(404).json({message : "The book was not found"});
-        }
-        return res.status(200).json({message : "Book deleted successfully"});
-
-    }catch(error){
-        console.log(error.message);
-        res.status(500).json({message : error.message});
+    if (!book) {
+      return res
+        .status(404)
+        .json({ message: `The book with id : ${id} is not found` });
     }
-})
+    return res.status(200).json({ message: "Book updated" });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+//To delete a book by id
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const books = await Book.findByIdAndDelete(id);
+
+    if (!books) {
+      return res.status(404).json({ message: "The book was not found" });
+    }
+    return res.status(200).json({ message: "Book deleted successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
 
 mongoose
   .connect(process.env.mongoDB)
--- frontend/book-store/src/pages/Home.jsx
+++ frontend/book-store/src/pages/Home.jsx
@@ -10,72 +10,93 @@ import SearchItem from "../components/SearchItem.jsx";
 const Home = () => {
   const [books, setBooks] = useState([]);
   const [load, setLoad] = useState(false);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
+  const [totalCount, setTotalCount] = useState(0);
 
   useEffect(() => {
     setLoad(true);
     axios
-      .get("http://localhost:8000/books")
+      .get(`http://localhost:8000/books?page=${page}&limit=4&search=${search}`)
       .then((res) => {
         setBooks(res.data.dataList);
+        setTotalCount(res.data.totalCount);
         setLoad(false);
       })
       .catch((error) => console.log(error));
-  },[]);
+  }, [page, search]);
 
-  
   return (
     <div className="container">
-        
       <div className="Home">
         <h1>Book List</h1>
         <div className="SearchAndAdd">
-          <SearchItem 
-            search = {search}
-            setSearch = {setSearch}
-          />
-          <Link to="/books/create" className="addParent"> 
+          <SearchItem search={search} setSearch={setSearch} />
+          <Link to="/books/create" className="addParent">
             <MdOutlineAddBox className="AddBox" />
             Add new Book
-          </Link> 
+          </Link>
         </div>
         {load ? (
           <Spinner />
         ) : (
-          <table className="table">
-            <thead>
-              <tr>
-                <th>No</th>
-                <th>Title</th>
-                <th>Author</th>
-                <th>Published Year</th>
-                <th>Operations</th>
-              </tr>
-            </thead>
-            <tbody>
-              {books.filter((book) => book.title.toLowerCase().includes(search.toLowerCase())).map((book, index) =>
-                <tr key={book._id}>
-                  <td>{index + 1}</td>
-                  <td>{book.title}</td>
-                  <td>{book.author}</td>
-                  <td>{book.publishYear}</td>
-                  <td>
-                    <div className="operationButtons">
-                      <Link to={`/books/details/${book._id}`}>
-                        <BsInfoCircle className="iButton" />
-                      </Link>
-                      <Link to={`/books/edit/${book._id}`}>
-                        <AiOutlineEdit className="editButton" />
-                      </Link>
-                      <Link to={`/books/delete/${book._id}`}>
-                        <MdOutlineDelete className="deleteButton" />
-                      </Link>
-                    </div>
-                  </td>
+          <>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Title</th>
+                  <th>Author</th>
+                  <th>Published Year</th>
+                  <th>Operations</th>
                 </tr>
+              </thead>
+              <tbody>
+                {books.map((book, index) => (
+                  <tr key={book._id}>
+                    <td>{index + 1}</td>
+                    <td>{book.title}</td>
+                    <td>{book.author}</td>
+                    <td>{book.publishYear}</td>
+                    <td>
+                      <div className="operationButtons">
+                        <Link to={`/books/details/${book._id}`}>
+                          <BsInfoCircle className="iButton" />
+                        </Link>
+                        <Link to={`/books/edit/${book._id}`}>
+                          <AiOutlineEdit className="editButton" />
+                        </Link>
+                        <Link to={`/books/delete/${book._id}`}>
+                          <MdOutlineDelete className="deleteButton" />
+                        </Link>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <footer>
+              {page > 1 ? (
+                <button
+                  className="prevPage"
+                  onClick={() => {
+                    setPage(page - 1);
+                  }}
+                >
+                  Prev
+                </button>
+              ) : (
+                <></>
               )}
-            </tbody>
-          </table>
+              {page < Math.ceil(totalCount / 4) ? (
+                <button className="nextPage" onClick={() => setPage(page + 1)}>
+                  Next
+                </button>
+              ) : (
+                <></>
+              )}
+            </footer>
+          </>
         )}
       </div>
     </div>
