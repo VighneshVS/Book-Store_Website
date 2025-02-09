bf0a8b18725b5478fd6f8f51dbe94fcce643efcc
VighneshVS <vighneshvs27@gmail.com>
2025-02-09 09:08:34 +0530

    Update DeleteBook.jsx

--- frontend/book-store/src/pages/DeleteBook.jsx
+++ frontend/book-store/src/pages/DeleteBook.jsx
@@ -11,7 +11,7 @@ const DeleteBook = () => {
   const handleDelete =() =>{
     setLoad(true);
     axios
-        .delete(`http://localhost:8000/books/${id}`)
+        .delete(`book-store-dusky-sigma.vercel.app/books/${id}`)
         .then(()=>{
           setLoad(false);
           navigate('/');
